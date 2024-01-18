90deb3cd1d3ef24fc4318166804b3a3a7d1fe038
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2024-01-17 23:06:29 -0500

    Entropia celdas alrededor

--- sketch.js
+++ sketch.js
@@ -18,28 +18,28 @@ const reglas = [
     LEFT: 0,
   },
   {
-    //tile1
+    //tile 1
     UP: 1,
-    RIGTH: 1,
+    RIGTH: 4,
     DOWN: 0,
     LEFT: 0,
   },
   {
-    //tile2
+    //tile 2
     UP: 2,
     RIGTH: 0,
     DOWN: 0,
-    LEFT: 1,
+    LEFT: 4,
   },
   {
-    //tile3
+    //tile 3
     UP: 0,
     RIGTH: 3,
     DOWN: 2,
     LEFT: 0,
   },
   {
-    //tile4
+    //tile 4
     UP: 0,
     RIGTH: 0,
     DOWN: 0,
@@ -71,18 +71,18 @@ function setup() {
       opciones: opcionesI,
     };
   }
-  celdas[8].colapsada = true;
-  celdas[3].colapsada = true;
+  // celdas[8].colapsada = true;
+  // celdas[3].colapsada = true;
 
-  celdas[12].opciones = [5, 6, 8];
-  celdas[4].opciones = [4, 7, 12];
-  celdas[6].opciones = [9, 7, 12];
-  celdas[1].opciones = [6, 4, 8, 10];
-  celdas[5].opciones = [11, 6, 4, 8, 10];
+  // celdas[12].opciones = [5, 6, 8];
+  // celdas[4].opciones = [4, 7, 12];
+  // celdas[6].opciones = [9, 7, 12];
+  // celdas[1].opciones = [6, 4, 8, 10];
+  // celdas[5].opciones = [11, 6, 4, 8, 10];
 }
 
 function draw() {
-  background(111);
+  //background(111);
   const celdasDisponibles = celdas.filter((celda) => celda.colapsada == false);
   if (celdasDisponibles.length > 0) {
     celdasDisponibles.sort((a, b) => {
@@ -98,18 +98,72 @@ function draw() {
     const opcionSeleccionada = random(celdaSeleccionada.opciones);
     celdaSeleccionada.opciones = [opcionSeleccionada];
 
-    print(celdaSeleccionada);
+    //print(celdaSeleccionada);
 
     for (let x = 0; x < RETICULA; x++) {
       for (let y = 0; y < RETICULA; y++) {
         const celdaIndex = x + y * RETICULA;
         const celdaActual = celdas[celdaIndex];
         if (celdaActual.colapsada) {
-          image(azulejos[celdaActual.opciones[0]], x, y);
+          const indiceDeAzulejo = celdaActual.opciones[0];
+          const reglasActuales = reglas[indiceDeAzulejo];
+          //print(reglasActuales);
+
+          image(azulejos[indiceDeAzulejo], x * ancho, y * alto, ancho, alto);
+
+          // Cambiar entropia UP
+          if (y > 0) {
+            const indiceUP = x + (y - 1) * RETICULA;
+            const celdaUP = celdas[indiceUP];
+            if (!celdaUP.colapsada) {
+              cambiarEntropia(celdaUP, reglasActuales["UP"], "DOWN");
+            }
+          }
+          // Cambiar entropia RIGTH
+          if (x < RETICULA - 1) {
+            const indiceRIGTH = x + 1 + y * RETICULA;
+            const celdaRIGTH = celdas[indiceRIGTH];
+            if (!celdaRIGTH.colapsada) {
+              cambiarEntropia(celdaRIGTH, reglasActuales["RIGTH"], "LEFT");
+            }
+          }
+
+          //Cambiar entropia DOWN
+          if (y < RETICULA - 1) {
+            const indiceDOWN = x + (y + 1) * RETICULA;
+            const celdaDOWN = celdas[indiceDOWN];
+            if (!celdaDOWN.colapsada) {
+              cambiarEntropia(celdaDOWN, reglasActuales["DOWN"], "UP");
+            }
+          }
+
+          // Cambiar entropia LEFT
+          if (x > 0) {
+            const indiceLEFT = x - 1 + y * RETICULA;
+            const celdaLEFT = celdas[indiceLEFT];
+            if (!celdaLEFT.colapsada) {
+              cambiarEntropia(celdaLEFT, reglasActuales["LEFT"], "RIGTH");
+            }
+          }
+        } else {
+          strokeWeight(6);
+          rect(x * ancho, y * alto, ancho, alto);
         }
       }
     }
   }
 
   noLoop();
+}
+
+function cambiarEntropia(_celda, _regla, _opuesta) {
+  const nuevasOpciones = [];
+  for (let i = 0; i < _celda.opciones.length; i++) {
+    if (_regla == reglas[_celda.opciones[i]][_opuesta]) {
+      const celdaCompatible = _celda.opciones[i];
+      nuevasOpciones.push(celdaCompatible);
+    }
+  }
+  _celda.opciones = nuevasOpciones;
+  print(nuevasOpciones);
 }
